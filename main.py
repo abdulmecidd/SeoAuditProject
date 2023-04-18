3c75b36a213e5b4dd91696e6365ffa0fcb393fe9
Abdulmecid Demirhan <abdulmecid@cross4cloud.com>
2023-04-19 01:04:08 +0300

    open or create a worksheet block was created

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ from bs4 import BeautifulSoup
 sitemap_url = input('What is the sitemap URL of your website? \n')
 
 gc = gspread.service_account(filename="api.json")
+sh_name = input("What is the name of the worksheet you want to create? \n")
+
+# create or open a worksheet with the specific name
+try: 
+    sh = gc.open(sh_name)
+    worksheet = sh.sheet1
+except gspread.exceptions.SpreadsheetNotFound:
+    sh = gc.create(sh_name)
+    worksheet = sh.sheet1
+
