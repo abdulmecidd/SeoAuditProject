6420a0a4f89b6bf7e73a224d4e6a1c8906402675
Abdulmecid Demirhan <abdulmecid@cross4cloud.com>
2023-04-19 23:39:39 +0300

    Audits added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import xml.etree.ElementTree as ET
 import gspread
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 sitemap_url = input('What is the sitemap URL of your website? \n')
 
@@ -17,10 +18,11 @@ except gspread.exceptions.SpreadsheetNotFound:
     worksheet = sh.sheet1
 
 # create the table and headers
-headers = ["URL", "Title Tags", "Meta Description", "H1 Tags", "H2-H6 Tags", "Breadcrumbs", "Internal Linking", "Structured Data", "Social Media Tags", "Canonical URLs" "Www Vs. Non-www", "Http:// vs Https://", "Proper HTTP Status Code"]
+headers = ["URL", "Title Tags", "Meta Description", "H1 Tags", "H2-H6 Tags", "Internal Linking", "Social Media Tags", "Canonical URLs" "Www Vs. Non-www", "Http:// vs Https://", "Proper HTTP Status Code"]
 header_range = "A1:N1"
 worksheet.update(header_range, [headers])
 worksheet.format(header_range, {"horizontalAlignment": "Center", "textFormat": {"bold": True}})
+
 
 def scrape_url(url, worksheet):
     response = requests.get(url)
@@ -29,35 +31,62 @@ def scrape_url(url, worksheet):
 
     ## is there title tag?
     title_tag = soup.find('title')
+
     if title_tag:
+        row.append("yes")
+
+    else:
+        row.append("no")
+
+    meta_tags = soup.find_all("meta")
+
+    if meta_tags:
         row.append("yes")
     else:
         row.append("no")
-    
-    ## are there meta tags?
-    meta_tags = soup.find_all('meta')
-    for tag in meta_tags:
-        if tag.get('name') == 'description':
-            row.append("yes")
-            break
-        else:
-            row.append("no")
-
-    ## are there h1 tags?
+      
     h1_tags = soup.find('h1')
+
     if h1_tags:
         row.append("yes")
     else:
-        row.append("no")
+        row.append('no')
 
-    # are there h2 tags?
-    h2_tags = soup.find_all(['h2', 'h3', 'h4', 'h5', 'h6'])
-    if h2_tags:
+    h2h3_tags = soup.find_all(['h2', 'h3', 'h4', 'h5', 'h6'])
+
+    if h2h3_tags:
         row.append("yes")
     else:
         row.append("no")
 
+    internalLinks = soup.find_all('a')
+    if internalLinks:
+        row.append("yes")
+    else:
+        row.append("no")
+
+    for tag in meta_tags:
+        if tag.get('property') == 'og:title' or tag.get('property') == 'og:image':
+            row.append("yes")
+            break
+    
+    row.append('no') if len(row) == 1 else None    
+
+    parsed_url = urlparse(url)
+    if parsed_url.netloc.startswith("www."):
+        row.append("URL starts with www")
+    else:
+        row.append("None www")
+    
+    if parsed_url.netloc.startswith("https://"):
+        row.append("URL starts with https://")
+    else:
+        row.append("Url starts with http://")
+
+    row.append(response.status_code)
+
     worksheet.append_row(row)
+    print(row)
 
 #function to scrape all URLs in the sitemap and its sub-sitemaps
 
@@ -70,9 +99,13 @@ def scrape_sitemap(sitemap_url, worksheet):
         alt_sitemap_url = element.text
         scrape_sitemap(alt_sitemap_url, worksheet)
 
-    for element in sitemap_xml.findall('.//{http://www.sitemaps.org/schemas/sitemap/0.9}sitemap/{http://www.sitemaps.org/schemas/sitemap/0.9}loc'):
+    for element in sitemap_xml.findall('.//{http://www.sitemaps.org/schemas/sitemap/0.9}url/{http://www.sitemaps.org/schemas/sitemap/0.9}loc'):
         url = element.text
         scrape_url(url, worksheet)
 
+#call the function to start scraping
 
+scrape_sitemap(sitemap_url, worksheet)
+
+print("scraping is complete!")
 
