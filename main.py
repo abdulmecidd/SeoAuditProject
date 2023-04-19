b809eb3dd4bb1d45ad3fa2a8e889bb1db703a6bb
Abdulmecid Demirhan <abdulmecid@cross4cloud.com>
2023-04-19 21:52:38 +0300

    scrape_url function was added

--- main.py
+++ main.py
@@ -22,4 +22,40 @@ header_range = "A1:N1"
 worksheet.update(header_range, [headers])
 worksheet.format(header_range, {"horizontalAlignment": "Center", "textFormat": {"bold": True}})
 
+def scrape_url(url, worksheet):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    row = [url]
+
+    ## is there title tag?
+    title_tag = soup.find('title')
+    if title_tag:
+        row.append("yes")
+    else:
+        row.append("no")
+    
+    ## are there meta tags?
+    meta_tags = soup.find_all('meta')
+    for tag in meta_tags:
+        if tag.get('name') == 'description':
+            row.append("yes")
+            break
+        else:
+            row.append("no")
+
+    ## are there h1 tags?
+    h1_tags = soup.find('h1')
+    if h1_tags:
+        row.append("yes")
+    else:
+        row.append("no")
+
+    # are there h2 tags?
+    h2_tags = soup.find_all(['h2', 'h3', 'h4', 'h5', 'h6'])
+    if h2_tags:
+        row.append("yes")
+    else:
+        row.append("no")
+
+    
 
